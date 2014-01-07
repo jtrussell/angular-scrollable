1fe5d45beaeb52a11488403ecafa88a15948620f
Justin <jus.russell@gmail.com>
2014-01-07 15:57:19 -0500

    fix: Allow element time to set height
    Otherwise elements pulled in via routes or ngIncludes won't get scollbars on
    hover until a user scrolls their mousewheel over them the first time.

--- dist/scrollable.js
+++ dist/scrollable.js
@@ -11,7 +11,7 @@
 angular.module('scrollable', []);
 
 angular.module('scrollable').directive('scrollable', [
-    'scrollableOptions', function(scrollableOptions) {
+    'scrollableOptions', '$timeout', function(scrollableOptions, $timeout) {
 
   'use strict';
 
@@ -31,7 +31,18 @@ angular.module('scrollable').directive('scrollable', [
         var opts = attrs.scrollable ? scope.$eval(attrs.scrollable) : {};
         opts = angular.extend(scrollableOptions.get(), opts);
 
-        element.perfectScrollbar(opts);
+        /**
+         * A bit of a hack, we need the underlying elements to have their
+         * heights calculated first? Possible related to:
+         * @see https://github.com/noraesae/perfect-scrollbar/issues/65
+         *
+         * Seems to be only needed when the item element getting hit with this
+         * directive is being pulled in via a route, ngInclude, or otherwise
+         * isn't on the initial page load.
+         */
+        $timeout(function() {
+          element.perfectScrollbar(opts);
+        });
       });
     }
   };
--- src/scripts/directives/scrollable.js
+++ src/scripts/directives/scrollable.js
@@ -1,5 +1,5 @@
 angular.module('scrollable').directive('scrollable', [
-    'scrollableOptions', function(scrollableOptions) {
+    'scrollableOptions', '$timeout', function(scrollableOptions, $timeout) {
 
   'use strict';
 
@@ -19,7 +19,18 @@ angular.module('scrollable').directive('scrollable', [
         var opts = attrs.scrollable ? scope.$eval(attrs.scrollable) : {};
         opts = angular.extend(scrollableOptions.get(), opts);
 
-        element.perfectScrollbar(opts);
+        /**
+         * A bit of a hack, we need the underlying elements to have their
+         * heights calculated first? Possible related to:
+         * @see https://github.com/noraesae/perfect-scrollbar/issues/65
+         *
+         * Seems to be only needed when the item element getting hit with this
+         * directive is being pulled in via a route, ngInclude, or otherwise
+         * isn't on the initial page load.
+         */
+        $timeout(function() {
+          element.perfectScrollbar(opts);
+        });
       });
     }
   };
