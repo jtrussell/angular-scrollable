e71aced2ee9be8554776caaebe1b51cfc1118b28
Justin <jus.russell@gmail.com>
2014-01-17 08:15:24 -0500

    fix: Scroll to top when container height changes
    Might not be desirable in all cases but for now we're addressing what happens
    when using `max-[height|width]`, scrolling, then filtering/trimming your list.
    You can end up with a seemingly empty list with all items off the page.

--- dist/scrollable.js
+++ dist/scrollable.js
@@ -27,13 +27,32 @@ angular.module('scrollable').directive('scrollable', [
         // We'll use our own scrollbars thank you very much
         element.css('overflow', 'hidden');
 
-        // Make it so
         var opts = attrs.scrollable ? scope.$eval(attrs.scrollable) : {};
         opts = angular.extend(scrollableOptions.get(), opts);
 
         /**
+         * Avoid some awkwardness when container dimentions change
+         *
+         * It's possible to lose sight of all elements using
+         * max-[height|width] and filtering the list of items.
+         *
+         * @todo How to better handle scrolling + filtering?
+         */
+        scope.$watch(function() {
+          return element.height();
+        }, function() {
+          element.scrollTop(0);
+        });
+
+        scope.$watch(function() {
+          return element.width();
+        }, function() {
+          element.scrollLeft(0);
+        });
+
+        /**
          * A bit of a hack, we need the underlying elements to have their
-         * heights calculated first? Possible related to:
+         * heights calculated first? Possibly related to:
          * @see https://github.com/noraesae/perfect-scrollbar/issues/65
          *
          * Seems to be only needed when the item element getting hit with this
